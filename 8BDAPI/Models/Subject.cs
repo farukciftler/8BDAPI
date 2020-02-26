71e001ef1ec3e8b603d19a546c5858b92bf3fead
Abdullah Faruk ÇİFTLER <37593465+farukciftler@users.noreply.github.com>
2020-02-26 18:32:30 +0300

    last settings

--- 8BDAPI/Models/Subject.cs
+++ 8BDAPI/Models/Subject.cs
@@ -14,5 +14,21 @@ namespace _8BDAPI.Models
         public DateTime createDate { get; set; }
         public DateTime updateDate { get; set; }
         public int BSHIU { get; set; } //BOLD STEADY HIDE ITALIC UNDERLINE
+
+
+        /*
+         CREATE TABLE subject(
+         id   INT              NOT NULL,
+         author_id   INT              NOT NULL,
+         subject   string              NOT NULL,
+         isActive int     NOT NULL,
+         createDate  datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+         updateDate  datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+         BSHIU int NOT NULL,
+         PRIMARY KEY (id)
+         );
+         
+         
+         */
     }
 }
