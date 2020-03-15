8061cdeeeadf5976f5a6d31c17c5edd57055feab
Abdullah Faruk ÇİFTLER <37593465+farukciftler@users.noreply.github.com>
2020-03-15 23:36:12 +0300

    sql changed

--- 8BDAPI.sql
+++ 8BDAPI.sql
@@ -207,3 +207,13 @@ USE [master]
 GO
 ALTER DATABASE [8BDAPI] SET  READ_WRITE 
 GO
+INSERT INTO [8BDAPI].dbo.userlevel (userLevel,userLevelInfo) VALUES 
+(1,'guilty')
+,(2,'abroad')
+,(3,'newbie')
+,(4,'bot')
+,(5,'author')
+,(6,'moderator')
+,(7,'administrator')
+,(8,'developer')
+GO
---
+++ userlevel.sql
@@ -0,0 +1,10 @@
+INSERT INTO [8BDAPI].dbo.userlevel (userLevel,userLevelInfo) VALUES 
+(1,'guilty')
+,(2,'abroad')
+,(3,'newbie')
+,(4,'bot')
+,(5,'author')
+,(6,'moderator')
+,(7,'administrator')
+,(8,'developer')
+; 
